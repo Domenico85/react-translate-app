9775c54074aeb04231e2abf5476c262e9b4c22a6
Dome <arillotta@gmail.com>
2025-02-13 14:41:24 +0100

    added login

---
+++ src/components/Login.jsx
@@ -0,0 +1,44 @@
+// src/components/Login.jsx
+import React, { useState } from 'react';
+import { loginUser } from '../services/authService';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await loginUser(email, password);
+     
+      window.location.href = "/translator";
+    } catch (error) {
+      setErrorMessage("Error signing in: " + error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button type="submit">Login</button>
+      </form>
+      {errorMessage && <p>{errorMessage}</p>}
+    </div>
+  );
+};
+
+export default Login;

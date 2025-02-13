367ec1382a350ced82505e505db56c2521933386
Dome <arillotta@gmail.com>
2025-02-13 14:57:00 +0100

    cleaning code

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
-// src/components/Login.jsx
-import React, { useState } from 'react';
+
+import { useState } from 'react';
 import { loginUser } from '../services/authService';
 
 const Login = () => {
